951b36c47850591da8d4266cdc4953786792594e
srcalexander <221738446+srcalexander@users.noreply.github.com>
2025-08-05 23:57:39 +0530

    Practiced creating functions and calling a function based on conditional statements

---
+++ learning-javascript/passingfunctions.js
@@ -0,0 +1,42 @@
+function sum(x, y) {
+   console.log(x + y);
+   return x + y;
+}
+
+function divide(x, y) {
+   console.log(x / y);
+   return x / y;
+}
+
+function multiply(x, y) {
+   console.log(x * y);
+   return x * y;
+}
+
+function subtract(x, y) {
+   console.log(x - y);
+   return x - y;
+}
+
+function run(operation, x, y) {
+   if (operation === "sum") {
+      return sum(x, y);
+   }
+
+   if (operation === "divide") {
+      return divide(x, y);
+   }
+
+   if (operation === "multiply") {
+      return multiply(x, y);
+   }
+
+   if (operation === "subtract") {
+      return subtract(x, y);
+   }
+}
+
+run("sum", 1, 2);
+run("divide", 12, 3);
+run("subtract", 10, 3);
+run("multiply", 4, 3);
