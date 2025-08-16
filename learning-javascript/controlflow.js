1d647f17bb6d216a3eddf1460ebc37cec68673d9
srcalexander <221738446+srcalexander@users.noreply.github.com>
2025-08-16 23:50:16 +0100

    Continued with FM bootcamp javascript. Learned about for and while loops

---
+++ learning-javascript/controlflow.js
@@ -0,0 +1,19 @@
+const isSkyBlue = true;
+
+if (isSkyBlue) {
+   console.log("The sky is blue");
+} else {
+   console.log("The sky is not blue");
+}
+
+let counter = 0;
+
+while (counter < 10) {
+   counter++;
+   console.log(counter);
+}
+
+for (let i = 0; i < 20; i++) {
+   counter++;
+   console.log(counter);
+}
